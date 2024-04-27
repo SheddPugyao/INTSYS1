e3f5046a61e2a6bd2830f149224586be2da8dc13
ciper <20215816@e.ubaguio.edu>
2024-04-27 21:12:03 +0800

    comments

--- botv2.py
+++ botv2.py
@@ -23,3 +23,15 @@ while True:
     print("EnrollmentBot: ", end="")
     for line in response_lines:
         print(line)
+
+#possible queries: (from enrollment_data.py)
+#Hi, I'm interested in enrolling for a program. What options do you have?
+# What's the deadline for enrollment?
+# Can you tell me about the reservation procedures for incoming freshmen?
+# What are the admission requirements for incoming freshmen?
+# Hi, I'm a new student. How do I enroll for courses?
+# What are the subjects for Associate in Computer Technology in the first semester of the first year?
+# What are the subjects for Multimedia and Web Design in the first semester of the first year?
+# What are the subjects for Information Technology in the first semester of the first year?
+# What are the subjects for Computer Engineering in the first semester of the first year?
+# What are the subjects for Computer Science in the first semester of the first year?
